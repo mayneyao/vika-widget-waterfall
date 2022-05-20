a11e98c2d05c3568d1aa1d20488e6b900efd8672
mayne <i@gine.me>
2022-05-20 13:41:08 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,69 @@
+import { initializeWidget, useActiveViewId, useFields, useRecords } from '@vikadata/widget-sdk';
 import React from 'react';
-import { initializeWidget } from '@vikadata/widget-sdk';
-import { Setting } from './setting';
+import { Chart } from "react-google-charts";
+
+
 
 export const HelloWorld: React.FC = () => {
+  const viewId = useActiveViewId()
+  const records = useRecords(viewId)
+  const fields = useFields(viewId);
+  const fieldNameIdMap = fields.reduce((acc, field) => {
+    acc[field.name] = field.id;
+    return acc;
+  }, {});
+
+  const columns = [
+    { type: "string", label: "Task ID" },
+    { type: "string", label: "Task Name" },
+    { type: "string", label: "Resource" },
+    { type: "date", label: "Start Date" },
+    { type: "date", label: "End Date" },
+    { type: "number", label: "Duration" },
+    { type: "number", label: "Percent Complete" },
+    { type: "string", label: "Dependencies" },
+  ];
+
+
+
+  const options = {
+    // width: 2000,
+    height: 2000,
+    gantt: {
+      defaultStartDate: null,
+    }
+    // gantt: {
+    //   defaultStartDateMillis: 1653016820508,
+    // },
+  };
+
+  const rows = records.map(record => {
+    const start = new Date(record.getCellValue(fieldNameIdMap['startAt']))
+    const end = new Date(record.getCellValue(fieldNameIdMap['endAt']))
+    return [
+      record.getCellValue(fieldNameIdMap['id']),
+      record.getCellValue(fieldNameIdMap['name']),
+      record.getCellValue(fieldNameIdMap['resourceId']),
+      start,
+      end,
+      end.getTime() - start.getTime(),
+      100, //record.getCellValue(fieldNameIdMap['percent']),
+      null,//record.getCellValue(fieldNameIdMap['dependencies']),
+    ]
+  })
+
+  const data = [columns, ...rows];
+
   return (
-    <div style={{ display: 'flex', height: '100%' }}>
-      <div style={{ flexGrow: 1, overflow: 'auto', padding: '0 8px'}}>
-        hello，world 🚀
-      </div>
-      <Setting />
+    <div style={{ width: '100%', height: '100%', overflow: 'scroll' }}>
+      <Chart
+        chartType="Gantt"
+        width="100%"
+        height="100%"
+        data={data}
+        options={options}
+      />
+      {/* <Setting /> */}
     </div>
   );
 };
